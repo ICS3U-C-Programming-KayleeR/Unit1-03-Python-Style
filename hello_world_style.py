6365a8f347c14d4d7c53e1b2923823e4530d142c
Kaylee Ralejoe <lintle.ralejoe@stu.ocsb.ca>
2026-09-15 14:23:22 +0000

    First commit of hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Created By: Kaylee Ralejoe
+# Date: 14,09,2026
+# Write what this program does.
+def main():
+    print("Hello kaylee")
+    print("It's a party everyday!!")
+
+if __name__ == "__main__":
+    main()
